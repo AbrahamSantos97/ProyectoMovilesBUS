f6f14b6091a5bfb4353ca4c0870f78b669013770
abrahamsantos <abraham_3.1416@hotmail.com>
2018-11-08 16:59:29 -0600

    gg

--- app/src/main/java/com/abrahamsantos/plogin/mapsV.java
+++ app/src/main/java/com/abrahamsantos/plogin/mapsV.java
@@ -10,7 +10,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -39,36 +41,54 @@ public class mapsV extends AppCompatActivity implements OnMapReadyCallback, Acti
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_v);
+        android.support.v7.widget.Toolbar barra = findViewById(R.id.toolbar);
+        setSupportActionBar(barra);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        puntos.add(new Camiones("Bolivar",19.4326077, -99.13320799999997));
+        /*puntos.add(new Camiones("Bolivar",19.4326077, -99.13320799999997));
         puntos.add(new Camiones("Boca del rio",19.4326077, -99.13320799999997));
         puntos.add(new Camiones("Norte sur",19.4326077, -99.13320799999997));
         puntos.add(new Camiones("Revolucion",19.4326077, -99.13320799999997));
-        puntos.add(new Camiones("Volcanes",19.4326077, -99.13320799999997));
+        puntos.add(new Camiones("Volcanes",19.4326077, -99.13320799999997));*/
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-       for (Camiones bus: puntos) {
+        /*for (Camiones bus: puntos) {
             LatLng punBus = new LatLng(bus.getLatitud(),bus.getLongitud());
             mMap.addMarker(new MarkerOptions().position(punBus).title(bus.getRuta()).icon(BitmapDescriptorFactory.fromResource(R.drawable.busstop)));
-        }
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(punBus,14));
+        }*/
+        LatLng parada1 = new LatLng(19.4326077,-99.13320799999997);
+        mMap.addMarker(new MarkerOptions().position(parada1).title("Parada1"));
+
+        LatLng parada2 = new LatLng(0.0,0.0);
+        mMap.addMarker(new MarkerOptions().position(parada2).title("Parada2"));
+
+        LatLng parada3 = new LatLng(10.845,-67.34563);
+        mMap.addMarker(new MarkerOptions().position(parada3).title("Parada3"));
+
+        //LatLng parada4 = new LatLng(19.4326077,-99.13320799999997);
+        //mMap.addMarker(new MarkerOptions().position(parada1).title("Parada1"));
         UbicacionUser();
         /*LatLng usuario = new LatLng(loc.getLatitude(), loc.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(usuario).title("Marker of Abraham"));
+        mMap.addMarker(new MarkerOptions().position(usuario).title("Marker of Abraham"));*/
 
 
-        LatLng bus = new LatLng(19.4326077, -99.13320799999997);
+        /*LatLng bus = new LatLng(19.4326077, -99.13320799999997);
         mMap.addMarker(new MarkerOptions().position(bus).title("Marker of Bus").icon(BitmapDescriptorFactory.fromResource(R.drawable.busstop)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(bus));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(usuario, 14));*/
+        /mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(usuario, 14));*/
     }
 
     private void AgregarMarcador(double Latitude, double Longitude) {
